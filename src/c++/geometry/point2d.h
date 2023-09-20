5c93a27665732e5e2ac2e26dd0eeea4f5ec4ae2a
whs31 <75864292+whs31@users.noreply.github.com>
2023-09-20 13:23:42 +0300

    Points

---
+++ src/c++/geometry/point2d.h
@@ -0,0 +1,39 @@
+//
+// Created by whs31 on 20.09.23.
+//
+
+#pragma once
+#include "LPVL/Point1D"
+
+namespace LPVL
+{
+  class Point2D : public Point1D
+  {
+    public:
+      explicit Point2D() noexcept;
+      explicit Point2D(float x, float y) noexcept;
+
+      [[nodiscard]] float y() const noexcept;   void setY(float) noexcept;
+      [[nodiscard]] float& refY() noexcept;
+
+    protected:
+      float m_y;
+  };
+} // LPVL
+
+namespace LPVL
+{
+  Point2D::Point2D() noexcept
+    : Point1D()
+    , m_y(0)
+  {}
+
+  Point2D::Point2D(float x, float y) noexcept
+    : Point1D(x)
+    , m_y(y)
+  {}
+
+  float Point2D::y() const noexcept { return m_y; }
+  void Point2D::setY(float q) noexcept { m_y = q; }
+  float& Point2D::refY() noexcept { return m_y; }
+} // LPVL

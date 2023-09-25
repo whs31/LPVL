304c27b5893fb6e984ec64704a93c3359a24a1a8
whs31 <75864292+whs31@users.noreply.github.com>
2023-09-25 12:33:05 +0300

    Numbers

--- src/c++/utils/mathf.h
+++ src/c++/utils/mathf.h
@@ -2,9 +2,18 @@
 
 #include <limits>
 #include <cmath>
+#include <type_traits>
+
+template<typename T>
+using enable_if_floating = std::enable_if_t<std::is_floating_point_v<T>, T>;
 
 namespace LPVL
 {
+  namespace Numbers
+  {
+    template<class T> inline constexpr T pi = enable_if_floating<T>(3.141592653589793238462643383279502884L);
+  }
+
   double log(double base, double value) noexcept { return (std::log(value) / std::log(base)); }
 
   double deg2rad(double degrees) noexcept { return degrees * (M_PI / 180); }

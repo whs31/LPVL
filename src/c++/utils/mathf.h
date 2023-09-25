1d49bbd347d9e5785c355ff534e73ad6ee63f50d
whs31 <75864292+whs31@users.noreply.github.com>
2023-09-25 14:32:27 +0300

    Update mathf.h

--- src/c++/utils/mathf.h
+++ src/c++/utils/mathf.h
@@ -23,13 +23,13 @@ namespace LPVL
     constexpr inline double φ = phi<double>;
   }
 
-  double log(double base, double value) noexcept { return (std::log(value) / std::log(base)); }
+  constexpr inline double log(double base, double value) noexcept { return (std::log(value) / std::log(base)); }
 
-  double deg2rad(double degrees) noexcept { return degrees * (Numbers::pi<double> / 180); }
-  double rad2deg(double radians) noexcept { return radians * (180 / Numbers::pi<double>); }
+  constexpr inline double deg2rad(double degrees) noexcept { return degrees * (Numbers::pi<double> / 180); }
+  constexpr inline double rad2deg(double radians) noexcept { return radians * (180 / Numbers::pi<double>); }
 
-  double NaN() noexcept;
-  bool isNaN(double other) noexcept;
+  constexpr inline double NaN() noexcept { return std::numeric_limits<double>::quiet_NaN(); };
+  constexpr inline bool isNaN(double other) noexcept { return std::isnan(other); };
 
   constexpr bool compare(double x, double y) { return (std::abs(x - y) * 1000000000000. <= std::min(std::abs(x), std::abs(y))); }
   constexpr bool compare(float x, float y) { return (std::abs(x - y) * 100000.f <= std::min(std::abs(x), std::abs(y))); }
